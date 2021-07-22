7485ffa9bf87196e5bdd63083404a3f58d9de4c1
aman gupta <amangupta89520@gmail.com>
2021-07-22 10:11:09 +0530

    adding files

---
+++ lib/views/signIn.dart
@@ -0,0 +1,24 @@
+import 'package:chat_app_1/widgets/widget.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    appBar: appBAR(),
+
+    body: Container(
+      
+    ),
+
+
+      
+    );
+  }
+}
---
+++ lib/widgets/widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget appBAR(){
+  return AppBar(
+    title: Image(
+      image: AssetImage("assets/images/logo.png"),
+      height: 50,
+    ),
+
+  );
+}
